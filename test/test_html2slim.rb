74b8e7220b117e80ce5ac5a8a2c7db79080075ba
Anton Chuchkalov <a2new@yandex.ru>
2017-03-08 14:55:50 +0300

    refactore test methods

--- test/test_html2slim.rb
+++ test/test_html2slim.rb
@@ -7,42 +7,20 @@ MiniTest.autorun
 
 class TestEEx2Slime < MiniTest::Test
   def setup
-    create_html_file
+    `touch #{html_file}`
   end
 
   def teardown
-    cleanup_tmp_files
+    FileUtils.rm_rf(tmp_dir)
   end
 
-  def test_id_and_class_rules
-    IO.popen("bin/html2slime test/fixtures/id_and_class_rules.html -", "r") do |f|
-      assert_equal File.read("test/fixtures/id_and_class_rules.slime"), f.read
+  def test_integrational
+    IO.popen("bin/html2slime test/fixtures/slim_lang.eex -", "r") do |f|
+      assert_equal File.read("test/fixtures/slim_lang.slime"), f.read
     end
-  end
 
-  def test_convert_slim_lang_html
-    IO.popen("bin/html2slime test/fixtures/slim-lang.html -", "r") do |f|
-      assert_equal File.read("test/fixtures/slim-lang.slime"), f.read
-    end
-  end
-
-  def test_convert_erb
-    IO.popen("bin/eex2slime test/fixtures/eex-example.html.eex -", "r") do |f|
-      assert_equal File.read("test/fixtures/eex-example.html.slime"), f.read
-    end
-  end
-
-  def test_convert_multiline_function
-    IO.popen("bin/eex2slime test/fixtures/multiline_function.eex -", "r") do |f|
-      assert_equal File.read("test/fixtures/multiline_function.slime"), f.read
-    end
-  end
-
-  # It would be cool to use better indentation for case clauses,
-  # but I don't know how to implement it with current gsub'y approach.
-  def test_convert_control_flow_blocks
-    IO.popen("bin/eex2slime test/fixtures/control_flow.eex -", "r") do |f|
-      assert_equal File.read("test/fixtures/control_flow.slime"), f.read
+    IO.popen("bin/eex2slime test/fixtures/eex_example.eex -", "r") do |f|
+      assert_equal File.read("test/fixtures/eex_example.slime"), f.read
     end
   end
 
@@ -66,45 +44,54 @@ class TestEEx2Slime < MiniTest::Test
     end
   end
 
-  def test_via_regular_ruby_call
-    actual = EEx2Slime.convert!("test/fixtures/control_flow.eex", :eex)
-    assert_equal File.read("test/fixtures/control_flow.slime").strip, actual
+  # It would be cool to use better indentation for case clauses,
+  # but I don't know how to implement it with current gsub'y approach.
+  def test_control_flow
+    assert_fixture_eex_to_slime("control_flow")
+  end
+
+  def test_id_and_class_rules
+    assert_fixture_eex_to_slime("id_and_class_rules")
+  end
+
+  def test_multiline_function
+    assert_fixture_eex_to_slime("multiline_function")
+  end
+
+  def test_nested
+    assert_fixture_eex_to_slime("nested")
   end
 
   def test_data_attributes
-    html = '<a href="test" data-param1="var" data-param2="(1 + 1)" data-param3="string"></a>'
-    slim = 'a[href="test" data-param1="var" data-param2="(1 + 1)" data-param3="string"]'
-    assert_html_to_slim html, slim
+    html = '<a href="test" data-param1="var" data-param2="(1 + 1)"></a>'
+    slim = 'a[href="test" data-param1="var" data-param2="(1 + 1)"]'
+    assert_eex_to_slime html, slim
   end
 
   def test_escaped_text
-    text = "this is js code sample.&nbsp; &raquo; &lt;script&gt;alert(0)&lt;/script&gt;"
-    assert_html_to_slim text, "| #{text}"
-    assert_erb_to_slim text, "| #{text}"
+    text = "this is js code sample.&nbsp; &raquo; &lt;script&gt;&lt;/script&gt;"
+    assert_eex_to_slime text, "| #{text}"
   end
 
   def test_with_leading_dash
-    assert_erb_to_slim '<%- test() %>', "- test()"
+    assert_eex_to_slime '<%- test() %>', "- test()"
   end
 
   private
 
-  def assert_html_to_slim(given_html, expected_slim)
-    actual_slim = EEx2Slime::HTMLConverter.new(given_html).to_s
-    assert_equal expected_slim, actual_slim
-  end
-
-  def assert_erb_to_slim(given_erb, expected_slim)
+  def assert_eex_to_slime(given_erb, expected_slim)
     actual_slim = EEx2Slime::EExConverter.new(given_erb).to_s
     assert_equal expected_slim, actual_slim
   end
 
-  def tmp_dir
-    @tmp_dir ||= Dir.mktmpdir("html2slime.")
+  def assert_fixture_eex_to_slime(fixture_name)
+    expected = File.read("test/fixtures/#{fixture_name}.slime").strip
+    actual = EEx2Slime.convert!("test/fixtures/#{fixture_name}.eex", :eex)
+    assert_equal(expected, actual)
   end
 
-  def create_html_file
-    `touch #{html_file}`
+  def tmp_dir
+    @tmp_dir ||= Dir.mktmpdir("html2slime.")
   end
 
   def html_file
@@ -113,9 +100,5 @@ class TestEEx2Slime < MiniTest::Test
 
   def erb_file
     File.join(tmp_dir, "dummy.html.eex")
-  end
-
-  def cleanup_tmp_files
-    FileUtils.rm_rf(tmp_dir)
   end
 end
